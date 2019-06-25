bd9618a09c1ed83ad43eefe908e7593a399b5182
Abhijeet <abhijeet.abhijeet@soprasteria.com>
2019-06-25 22:50:56 +0530

    Removed comments from renderer

--- GameChange/GameChange.Android/CustomRenderers/BorderlessEntryRenderer.cs
+++ GameChange/GameChange.Android/CustomRenderers/BorderlessEntryRenderer.cs
@@ -28,15 +28,7 @@ namespace GameChange.Droid.CustomRenderers
         {
             base.OnElementChanged(e);
             if (e.OldElement == null)
-            {
-                //Control.Background = null;
-
-                //var layoutParams = new MarginLayoutParams(Control.LayoutParameters);
-                ////layoutParams.SetMargins(0, 0, 0, 0);
-                ////LayoutParameters = layoutParams;
-                ////Control.LayoutParameters = layoutParams;
-                ////Control.SetPadding(0, 0, 0, 0);
-                ////SetPadding(0, 0, 0, 0);
+            {                
                 Control.SetTextColor(Android.Graphics.Color.DarkCyan);
             }
         }
