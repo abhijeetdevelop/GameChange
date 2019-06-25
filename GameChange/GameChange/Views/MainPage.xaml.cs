f74b077827443746a72936b2650f1675818eb69a
Abhijeet <abhijeet.abhijeet@soprasteria.com>
2019-06-25 19:03:31 +0530

    Add project files.

---
+++ GameChange/GameChange/Views/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using GameChange.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GameChange
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Create_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new CreatePageView());
+        }
+
+        private void View_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ListPageView());
+
+        }
+    }
+}
